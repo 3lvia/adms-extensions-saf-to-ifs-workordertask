8ffdbc425af96ce874ae939132b1b30fd22e5f76
Egil Ingvar Utne <egil.utne@elvia.no>
2022-10-13 16:30:26 +0200

    Minor changes

--- src/adms-extensions-saf-to-ifs-workordertask/IfsModel/IFSWorkOrder.cs
+++ src/adms-extensions-saf-to-ifs-workordertask/IfsModel/IFSWorkOrder.cs
@@ -10,28 +10,28 @@ namespace Model
     public class IFSWorkOrderBody
     {
         public string Sender { get; set; }
-        //public string Context { get; set; }
-        //public string ContextSub { get; set; }
-        //public string RegDate { get; set; }
-        //public string Objstate { get; set; }
-        //public string ExtRefKeyWo { get; set; }
-        //public string MchCode { get; set; }
-        //public string UuidObject { get; set; }
-        //public string ErrDescr { get; set; }
-        //public string ErrDescrLo { get; set; }
-        //public string WorkDescrLo { get; set; }
-        //public string LatestFinish { get; set; }
-        //public string ReportedBy { get; set; }
-        //public string CustomerNo { get; set; }
-        //public string UuidCustomer { get; set; }
-        //public string ReferenceNo { get; set; }
-        //public string PhoneNo { get; set; }
-        //public string Contact { get; set; }
-        //public string OrgCode { get; set; }
-        //public string WorkMasterSign { get; set; }
+        public string Context { get; set; }
+        public string ContextSub { get; set; }
+        public string RegDate { get; set; }
+        public string Objstate { get; set; }
+        public string ExtRefKeyWo { get; set; }
+        public string MchCode { get; set; }
+        public string UuidObject { get; set; }
+        public string ErrDescr { get; set; }
+        public string ErrDescrLo { get; set; }
+        public string WorkDescrLo { get; set; }
+        public string LatestFinish { get; set; }
+        public string ReportedBy { get; set; }
+        public string CustomerNo { get; set; }
+        public string UuidCustomer { get; set; }
+        public string ReferenceNo { get; set; }
+        public string PhoneNo { get; set; }
+        public string Contact { get; set; }
+        public string OrgCode { get; set; }
+        public string WorkMasterSign { get; set; }
 
-        //public InfoFields[] InfoFields { get; set; }
-        //public WorkTasks[] WorkTasks { get; set; }
+        public InfoFields[] InfoFields { get; set; }
+        public WorkTasks[] WorkTasks { get; set; }
     }
 
     public class InfoFields
