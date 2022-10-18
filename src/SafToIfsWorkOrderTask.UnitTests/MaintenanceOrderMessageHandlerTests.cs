e292edb9a3793b1821068a32bf33f569909afe58
computasjorn <102790578+computasjorn@users.noreply.github.com>
2022-10-18 14:41:27 +0200

    first test, just to create a unit test project

---
+++ src/SafToIfsWorkOrderTask.UnitTests/MaintenanceOrderMessageHandlerTests.cs
@@ -0,0 +1,49 @@
+using AutoMapper;
+using FakeItEasy;
+using MaintenanceOrderReader.MessageHandlers;
+using MaintenanceOrdersOutBound;
+using ServicesIfs;
+using Xunit;
+
+namespace SafToIfsWorkOrderTask.UnitTests;
+
+public class MaintenanceOrderMessageHandlerTests
+{
+    [Theory]
+    [InlineData("Hyse")]
+    [InlineData("<message>\n   <text>Hello, world!</text>\n</message>")]
+    public void HandleMessage_Throws_ForMalformedXMLs(string input)
+    {
+        // Arrange
+        var basicMocks = new BasicMocks();
+        var underTest = CreateUnderTest(basicMocks);
+
+        // Act
+        var actual = Record.Exception(() => underTest.HandleMessage(input));
+
+        // Assert
+        Assert.IsType<InvalidOperationException>(actual);
+    }
+
+    private MaintenanceOrderMessageHandler CreateUnderTest(BasicMocks basicMocks)
+    {
+        return new MaintenanceOrderMessageHandler(
+            basicMocks.IfsWorkOrder,
+            basicMocks.Mapper,
+            basicMocks.Soap);
+    }
+
+    private class BasicMocks
+    {
+        public readonly IIfsWorkOrder IfsWorkOrder;
+        public readonly IMapper Mapper;
+        public readonly IMaintenanceOrders_Port Soap;
+
+        public BasicMocks()
+        {
+            IfsWorkOrder = A.Fake<IIfsWorkOrder>();
+            Mapper = A.Fake<IMapper>();
+            Soap = A.Fake<IMaintenanceOrders_Port>();
+        }
+    }
+}
